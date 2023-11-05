465d469dd523e81b190db68324876b29088a763b
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2023-11-06 00:10:52 +0100

    registration rest api fixed response

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -1,7 +1,6 @@
 import User from "../../utils/models/User";
 import connectMongo from "../../utils/db/mongodb";
 import bcrypt from "bcrypt";
-import { redirect } from 'next/navigation'
 const saltRounds = 10;
 
 await connectMongo();
@@ -13,9 +12,13 @@ export default async function handler(req, res) {
 
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await User.create({ email, password: hashedPassword});
-    console.log('User created: ', user);
-    redirect('/login');
+    try {
+      const user = await User.create({ email, password: hashedPassword });
+      console.log('User created: ', user);
+    }catch(err) {
+      console.log(err);
+      return res.status(400).json({ message: "User already exists" });
+    }
+    return res.status(200).json({});
   }
 }
