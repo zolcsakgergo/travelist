e40d691eed4201a83e4a21faa31ee99f1fd508a0
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2023-12-02 16:52:42 +0100

    login jwt bug fix

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,23 +6,36 @@ import jwt from "jsonwebtoken";
 await connectMongo();
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const parsedBody = req.body;
-    const { email, password } = parsedBody;
+  try {
+    if (req.method === "POST") {
+      const parsedBody = req.body;
+      const { email, password } = parsedBody;
 
-    const user = await User.findOne({ email });
-    if (!user) {
+      const user = await User.findOne({ email });
+      if (!user) {
         return res.status(400).json({ message: "User not found" });
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
+        return res.status(500).json({ message: "JWT secret is not defined" });
+      }
+
+      const token = jwt.sign({ id: user._id }, process.env.NEXT_PUBLIC_JWT_SECRET, {
+        expiresIn: '5d'
+      });
+
+      res.setHeader("Set-Cookie", `token=${token}; path=/; HttpOnly`);
+
+      return res.status(200).json({});
     }
-
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
-    
-    const token = jwt.sign({ id: user._id }, process.env.NEXT_PUBLIC_JWT_SECRET, { expiresIn: '5d' });
-
-    res.setHeader("Set-Cookie", `token=${token}; path=/; HttpOnly}`);
-
-    return res.status(200).json({});
+  } catch (error) {
+    console.error("Login failed", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 }
---
+++ utils/models/Places.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from 'mongoose';
+
+const placeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: String,
+        required: true,
+    },
+    latitude: {
+        type: String,
+        required: true,
+    },
+    longitude: {
+        type: String,
+        required: true,
+    }
+});
+
+const Place = models.Place || model('Place', placeSchema);
+
+export default Place;
