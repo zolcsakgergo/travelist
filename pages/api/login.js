cb9d13a0ff4ff585cf10f7fa3b3d53ec2500d5b1
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2023-12-02 17:03:01 +0100

    login bugfix

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,18 +5,24 @@ import jwt from "jsonwebtoken";
 
 await connectMongo();
 
+
 export default async function handler(req, res) {
   try {
     if (req.method === "POST") {
       const parsedBody = req.body;
       const { email, password } = parsedBody;
 
+      console.log('Email:', email); // Check if email is being received
+
       const user = await User.findOne({ email });
+      console.log('User:', user); // Check the retrieved user object
+
       if (!user) {
         return res.status(400).json({ message: "User not found" });
       }
 
       const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      console.log('Password Correct:', isPasswordCorrect); // Check password comparison
 
       if (!isPasswordCorrect) {
         return res.status(400).json({ message: "Invalid credentials" });
